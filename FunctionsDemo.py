662900e1eb201e6840cf2d8ae949a927eae59642
sunnyt2722 <sunnyt135@gmail.com>
2025-01-11 00:22:51 +0000

    Updated Friends demo

--- FunctionsDemo.py
+++ FunctionsDemo.py
@@ -1,4 +1,16 @@
-def GreetMe():
-    print("Hello Sunny")
+friends_list = ["Rambo", "Rocky", "Stallion"]
+def hello():
+    print("Hello!!")
 
-GreetMe()
+def user_age_in_seconds():
+    user_age = int(input("Enter your age: "))
+    age_in_seconds = user_age * 365 * 24 * 60 * 60
+    print(f"Your age is seconds is {age_in_seconds}")
+
+def add_friends_in_list():
+    friend_name = input("Enter friend's name to append : ")
+    friends_list.append(friend_name)
+    print(friends_list)
+hello()
+user_age_in_seconds()
+add_friends_in_list()
