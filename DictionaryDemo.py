83776705e49c60676589620f5a29c654a09012dd
sunnyt2722 <sunnyt135@gmail.com>
2025-01-11 00:02:58 +0000

    Updated dictonary demo

--- DictionaryDemo.py
+++ DictionaryDemo.py
@@ -10,3 +10,23 @@ dict["lastName"] = ["Kumar"]
 
 print(dict)
 
+listOfFriends = [
+    {"name":"Joe", "age": 30},
+    {"name":"Ross", "age": 32},
+    {"name":"Chandler", "age": 27}
+]
+
+print(listOfFriends[1])
+
+listOfStudents = [
+    {"name": "Robert", "grade":90},
+    {"name": "Downy", "grade": 84},
+    {"name": "Jr.", "grade":31.3}
+]
+
+print(len(listOfStudents[1]))
+totalMarks = 0
+for grades in listOfStudents:
+    totalMarks += grades["grade"]
+
+print(totalMarks/len(listOfStudents))
